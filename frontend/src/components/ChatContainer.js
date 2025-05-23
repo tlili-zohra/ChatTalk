f408acb9cac8274b1fc3774749c138cba184fa78
Zohra Tlili <tlili.zohra12@gmail.com>
2025-05-23 02:40:57 +0200

    last one

--- frontend/src/components/ChatContainer.js
+++ frontend/src/components/ChatContainer.js
@@ -8,21 +8,7 @@ const ChatContainer = ({ fetchAgain, setFetchAgain }) => {
   //const { selectedChat } = useContext(AuthContext);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "12px 20px",
-        backgroundColor: "#ffffff",
-        width: "66%",
-        border: "1.5px solid #d1d5db",
-        borderRadius: "16px",
-        boxShadow: "0 6px 18px rgba(102, 126, 234, 0.1)",
-        fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
-        minHeight: "400px",
-      }}
-    >
+    <div className="chat-container">
       <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
     </div>
   );
